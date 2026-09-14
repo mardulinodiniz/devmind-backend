b0bc73107cb263b260aac30f2fd9fd3e5fbb7142
Mardulno <mardulinodiniz@gmail.com>
2026-09-14 14:25:01 +0100

    Adicionar endpoint webhook BitPay

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ app.use(cors());
 
 app.use(express.json());
 
+app.post('/api/webhooks/bitpay', (req, res) => {
+    console.log('Webhook BitPay recebido:');
+    console.log(req.body);
+
+    res.status(200).json({
+        recebido: true
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 // Rota inicial
